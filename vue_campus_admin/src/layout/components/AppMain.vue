3224f8d643e2bfd6f6033cf206f4ca703bd23113
Luxp <986762118@qq.com>
2023-10-18 22:35:14 +0900

    #vue remove watermark

--- vue_campus_admin/src/layout/components/AppMain.vue
+++ vue_campus_admin/src/layout/components/AppMain.vue
@@ -32,14 +32,16 @@ export default {
   },
   mounted() {
     const ttt = this.Crypto.get(
-          "Wuv7//QyvM5eAH6r6Yo3ng=="
+          // "Wuv7//QyvM5eAH6r6Yo3ng=="
+          // "1021002301,100101010111101"
         );
     watermark.load({ watermark_txt: ttt });
   },
   watch: {
     $route(route) {
         this.ttt = this.Crypto.get(
-          "JcPYFM5UAxqqLazbPKP8clN2geEWtOa/7P4nS4iJFvMeTCDKC0KgsFRoyO/kvjgmgzNB+L4ASDp26yz7hbhDxUpvtnLYSUgDUtII9L1Rxtk="
+          // "JcPYFM5UAxqqLazbPKP8clN2geEWtOa/7P4nS4iJFvMeTCDKC0KgsFRoyO/kvjgmgzNB+L4ASDp26yz7hbhDxUpvtnLYSUgDUtII9L1Rxtk="
+          "John"
         );
     },
   },
